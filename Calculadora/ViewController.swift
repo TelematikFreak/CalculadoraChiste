c55b26f75fe353e63d5389c4616667a9728e3fdc
Alex Moreno <alx.spain@gmail.com>
2019-11-05 13:11:26 +0000

    añadir picare

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource {
     let modelo: CalculadoraModelo = CalculadoraModelo()
     let vista: CalculadoraVista = CalculadoraVista()
     var controlador: CalculadoraControlador!
+    let operadores: [String] = ["suma","resta","multiplica","divide"]
+    var operatorSelected: String = "suma"
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return operadores.count
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return controlador.getHistorialCount()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return operadores[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        operatorSelected = operadores[row]
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,13 +51,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tabla.delegate = self
         tabla.dataSource = self
         tabla.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        operando.delegate = self
+        operando.dataSource = self
     }
 
-    @IBOutlet weak var operando: UITextField!
     @IBOutlet weak var num1: UITextField!
     @IBOutlet weak var num2: UITextField!
     @IBOutlet weak var resultado: UITextField!
     
+    @IBOutlet weak var operando: UIPickerView!
     @IBOutlet weak var tabla: UITableView!
     
     @IBAction func clear(_ sender: UIButton) {
@@ -48,12 +69,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     @IBAction func calcula(_ sender: UIButton) {
-        if operando.text == "" || num1.text == "" || num2.text == "" {
+        if operatorSelected == "" || num1.text == "" || num2.text == "" {
             resultado.text = "de qué coño vas colega?"
             view.backgroundColor = UIColor.black
         } else {
             do{
-                try resultado.text = controlador.realizarOperacion(operando.text!, num1.text!, num2.text!)
+                try resultado.text = controlador.realizarOperacion(operatorSelected, num1.text!, num2.text!)
                 if resultado.text == "inf" || resultado.text == "-inf" {
                     view.backgroundColor = UIColor.red
                     resultado.text = "te has hecho 💩"
